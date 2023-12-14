afaa265bdb64ea0f288f30be22b18f7d21d6cb19
Alexandr Sviridov <gruwwer@gmail.com>
2023-12-14 20:21:33 +0700

    Made TodoList jQuery task

---
+++ todo_list_jquery/todoListJQuery.js
@@ -0,0 +1,65 @@
+$(function () {
+    const todoList = $("#todo-list");
+    const newTodoTextField = $("#new-todo-text-field");
+
+    $("#add-todo-form").submit(function (e) {
+        e.preventDefault();
+
+        let newTodoText = newTodoTextField.val().trim();
+        newTodoTextField.removeClass("invalid");
+
+        if (newTodoText.length === 0) {
+            newTodoTextField.addClass("invalid");
+            return;
+        }
+
+        const newTodo = $("<li>").addClass("todo-item");
+
+        function setViewMode() {
+
+            newTodo.html(`<span class="todo-item-text"></span>
+                    <button class="delete-button" type="button">Delete</button>
+                    <button class="edit-button" type="button">Edit</button>`);
+
+            newTodo.find(".todo-item-text").text(newTodoText);
+
+            newTodo.find(".delete-button").click(function () {
+                newTodo.remove();
+            });
+
+            newTodo.find(".edit-button").click(function () {
+                newTodo.html(`<input type="text" class="edit-text-field">
+                    <button class="cancel-button" type="button">Cancel</button>
+                    <button class="save-button" type="button">Save</button>
+                    <div class="edit-error-massage-block"></div>
+                    <div class="error-message">Need to insert text</div>`);
+
+
+                const editTextField = newTodo.find(".edit-text-field");
+                editTextField.val(newTodoText);
+
+                newTodo.find(".cancel-button").click(function () {
+                    setViewMode();
+                });
+
+                newTodo.find(".save-button").click(function () {
+                    const changedTodoText = editTextField.val().trim();
+
+                    if (changedTodoText.length === 0) {
+                        newTodo.find(".edit-error-massage-block").addClass("invalid");
+                        newTodo.find(".edit-text-field").addClass("invalid");
+                        return;
+                    }
+
+                    newTodoText = changedTodoText;
+                    setViewMode();
+                })
+            });
+        }
+        setViewMode();
+
+        todoList.append(newTodo);
+
+        newTodoTextField.val("");
+    });
+});
