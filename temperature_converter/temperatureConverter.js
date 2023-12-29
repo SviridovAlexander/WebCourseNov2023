3200a987b928b0da2bc522c80a9d1f91220f15b1
Alexandr Sviridov <gruwwer@gmail.com>
2023-12-30 04:23:14 +0700

    Corrected input validation, creating temperature conversion functions, more descriptive names for variables, and eliminated unnecessary conversion in toFixed

--- temperature_converter/temperatureConverter.js
+++ temperature_converter/temperatureConverter.js
@@ -1,35 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const inputTextField = document.getElementById("input-text-field");
+    const inputTextField = document.getElementById("temperature-celsius-input-text-field");
     const outputTextBlock = document.querySelector(".output-text-block");
 
     document.getElementById("converter-form").addEventListener("submit", function (e) {
         e.preventDefault();
 
-        const inputText = inputTextField.value;
+        const temperatureCelsiusInputString = inputTextField.value;
 
-        if (inputText.length === 0) {
+        if (temperatureCelsiusInputString.length === 0) {
             outputTextBlock.textContent = "Please fill in the field";
             outputTextBlock.classList.add("error-message");
-            inputTextField.classList.add("input-field-error")
+            inputTextField.classList.add("input-field-error");
             return;
         }
 
-        if (isNaN(inputText)) {
+        if (isNaN(parseFloat(temperatureCelsiusInputString))) {
             outputTextBlock.textContent = "Incorrect input";
             outputTextBlock.classList.add("error-message");
-            inputTextField.classList.add("input-field-error")
+            inputTextField.classList.add("input-field-error");
             return;
         }
 
         inputTextField.value = "";
         outputTextBlock.classList.remove("error-message");
-        inputTextField.classList.remove("input-field-error")
+        inputTextField.classList.remove("input-field-error");
 
-        outputTextBlock.textContent = inputText +
+        outputTextBlock.textContent = temperatureCelsiusInputString +
             " °Celsius = " +
-            parseFloat((parseFloat(inputText) + 273.15).toFixed(3)) +
+            celsiusToKelvin(parseFloat(temperatureCelsiusInputString)) +
             " °Kelvin = " +
-            parseFloat((inputText * 1.8 + 32).toFixed(3)) +
+            celsiusToFahrenheit(parseFloat(temperatureCelsiusInputString)) +
             " °Fahrenheit";
     });
+
+    function celsiusToKelvin(celsius) {
+        return (celsius + 273.15).toFixed(3);
+    }
+
+    function celsiusToFahrenheit(celsius) {
+        return (celsius * 1.8 + 32).toFixed(3);
+    }
 });
