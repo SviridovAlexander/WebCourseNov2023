3618d7abb475819a17ad703ca211403e793e68f1
Alexandr Sviridov <gruwwer@gmail.com>
2023-12-30 04:21:04 +0700

    "Improve console output with explanatory text and rename 'finishNumber' to 'endNumber' for clarity."

--- arrays_task/ArrayTaskScript.js
+++ arrays_task/ArrayTaskScript.js
@@ -17,10 +17,10 @@
             .reduce((sum, number) => sum + number, 0);
     }
 
-    function getArrayWithNumbersFromTo(startNumber, finishNumber) {
+    function getArrayWithNumbersFromTo(startNumber, endNumber) {
         const array = [];
 
-        for (let i = startNumber; i <= finishNumber; i++) {
+        for (let i = startNumber; i <= endNumber; i++) {
             array.push(i);
         }
 
@@ -35,9 +35,9 @@
 
     const numbers = [1, 5, 10, -20, 30, 6, 7, 100, 13, 75];
 
-    console.log(sortDescending(numbers));
-    console.log(getFirstElements(numbers, 5));
-    console.log(getLastElements(numbers, 5));
-    console.log(calculateEvenNumbersSum(numbers));
-    console.log(getEvenNumbersSquaresArray(getArrayWithNumbersFromTo(1, 100)));
+    console.log("Sorted in descending order:", sortDescending(numbers));
+    console.log("First 5 elements:", getFirstElements(numbers, 5));
+    console.log("Last 5 elements:", getLastElements(numbers, 5));
+    console.log("Sum of even numbers:", calculateEvenNumbersSum(numbers));
+    console.log("Squares of even numbers from 1 to 100:", getEvenNumbersSquaresArray(getArrayWithNumbersFromTo(1, 100)));
 })();
